7c77a138f317fe5c8f954d9da4a86f3281b208b4
ArsalShaikh12 <101574226+ArsalShaikh12@users.noreply.github.com>
2022-05-21 11:19:12 +0500

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -35,7 +35,7 @@ void TicTacToe::showBoard()
 
 bool TicTacToe::isDraw()
 {
-    return (board[1] != '1' && board[2] != '2' && board[3] != '3' && board[4] != '4' && board[5] != '5' && board[6] != '6' && board[7] != '7' && board[8] != '8' && board[9] != '9');
+    return (board[0] != '1' && board[1] != '2' && board[2] != '3' && board[3] != '4' && board[4] != '5' && board[5] != '6' && board[6] != '7' && board[7] != '8' && board[8] != '9');
 }
 int TicTacToe::checkwin()
 {
